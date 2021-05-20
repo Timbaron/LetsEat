aa9cc3da558cd6a43790a42323e0344fb57ddbcd
Akiode Timothy <akiodetimothy2017@gmail.com>
2021-05-20 10:05:13 +0100

    Create Search form

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const APP_ID = 'dccd8b32';
+  const APP_KEY = "b0ccd1347d986107178a1513b98783c3";
+
+  const ExampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
   return (
-    <h1>Hello React</h1>
+    <div className="App">
+      <form action="" className="search-form">
+        <input type="text" className="search-bar"/>
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+    </div>
   );
 }
 
